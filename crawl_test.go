c66cd158b6ece4b29c78667781e2150306a0c103
J Fixby <github@jfixby.com>
2024-02-08 15:46:50 +0100

    stash

--- crawl_test.go
+++ crawl_test.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"github.com/jfixby/bchcrawler/crawl"
 	"github.com/jfixby/pin"
+	"os"
 	"testing"
 )
 
@@ -15,4 +17,45 @@ func TestCrawlAndSave(t *testing.T) {
 	}
 
 	pin.D("saved to", dir)
+}
+
+func TestCrawlListAndSave(t *testing.T) {
+
+	l, err := ReadList()
+	if err != nil {
+		t.Errorf("Error occurred: %v", err)
+	}
+	for _, url := range l {
+		dir, err := crawl.CrawlAndSave(url)
+		if err != nil {
+			t.Errorf("Error occurred: %v", err)
+		}
+
+		pin.D("saved to", dir)
+	}
+}
+
+func ReadList() ([]string, error) {
+	return ReadFileLines("prompts/farmap list.txt")
+}
+
+func ReadFileLines(filePath string) ([]string, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		lines = append(lines, line)
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return lines, nil
 }
